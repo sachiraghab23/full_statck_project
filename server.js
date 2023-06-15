06c31ed0776e43f0b19c10b0d2ba32d624aa68ab
sachiraghab <sachiraghab@gmail.com>
2023-06-15 19:25:21 +0530

    project to be submit

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const morgan = require('morgan');
-const dotenv = require('dotenv');
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 const app = express();
-const connectDB = require('./config/config');
+const connectDB = require("./config/config");
 const port = process.env.PORT || 8080;
+const path=require("path");
 
 // config dotenv
 dotenv.config()
@@ -13,28 +14,28 @@ connectDB();
 
 //middlewares
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //route
-app.get('/', (req, res) => {
-  res.send('<h2>Server working</h2>');
+app.get("/", (req, res) => {
+  res.send("<h2>Server working</h2>");
 });
 
-const productRoute = require('./routes/productRoute');
-const userRoute = require('./routes/userRoute');
-const orderRoute = require('./routes/orderRoute');
-app.use('/api/products/', productRoute)
-app.use('/api/users/', userRoute)
-app.use('/api/orders/order', orderRoute)
+const productRoute = require("./routes/productRoute");
+const userRoute = require("./routes/userRoute");
+const orderRoute = require("./routes/orderRoute");
+app.use("/api/products/", productRoute)
+app.use("/api/users/", userRoute)
+app.use("/api/orders/order", orderRoute)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 } else {
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello from Node server via nodemon</h1>')
+  app.get("/", (req, res) => {
+    res.send("<h1>Hello from Node server via nodemon</h1>")
   });
 }
 
